6841d67e11b351e62d36fed61475e6db910bce26
Mauricio-OC <murisc0@hotmail.com>
2022-08-01 15:55:03 -0300

    handle elefants requisito 2

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,10 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Testa se a funcao nao receber parametros retorna undefined', () => {
+    expect(handlerElephants()).toEqual(undefined);
+  });
+  it('Testa o resultado caso o primeiro parametro seja (names)', () => {
+    expect(handlerElephants('names')).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
+  });
+});

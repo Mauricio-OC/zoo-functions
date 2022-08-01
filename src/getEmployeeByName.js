31e4b19c21788e13b989b1f0e4f59c6c39bd9dea
Mauricio-OC <murisc0@hotmail.com>
2022-08-01 18:15:34 -0300

    getemploybyname

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -4,6 +4,9 @@ function getEmployeeByName(employeeName) {
   if (employeeName === undefined) {
     return {};
   }
+  const employe = data.employees;
+  return employe.find((objeto) =>
+    objeto.firstName === employeeName || objeto.lastName === employeeName);
 }
 
 module.exports = getEmployeeByName;

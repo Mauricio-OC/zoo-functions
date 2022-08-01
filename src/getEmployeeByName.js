8d01f62e36cd56e6f6d74dec0a3f0adaac635bcf
Mauricio-OC <murisc0@hotmail.com>
2022-08-01 18:04:04 -0300

    getemployeebyname

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
 }
 
 module.exports = getEmployeeByName;

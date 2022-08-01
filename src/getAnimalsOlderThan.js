a46e845346b5aea815c27da36f70ded446b3b240
Mauricio-OC <murisc0@hotmail.com>
2022-08-01 17:47:40 -0300

    getanimalolder

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
+const anima = data.species;
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const animal1 = anima.find((element) => (element.name === animal));
+  const idade = animal1.residents;
+  const AnimalIdade = idade.every((idades) => idades.age >= age);
+  return AnimalIdade;
 }
-
 module.exports = getAnimalsOlderThan;

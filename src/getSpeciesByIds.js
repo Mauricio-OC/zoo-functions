5d11b63a5d8575df317a7485523e654b56bb245b
Mauricio-OC <murisc0@hotmail.com>
2022-08-06 10:01:45 -0300

    getspecies finalizado

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,9 +1,12 @@
 const data = require('../data/zoo_data');
 
+const { species } = require('../data/zoo_data');
+
 function getSpeciesByIds(...ids) {
   if (ids.length === 0) {
     return [];
   }
+  return ids.map((id) => species.find((element) => element.id === id));
 }
 
 module.exports = getSpeciesByIds;

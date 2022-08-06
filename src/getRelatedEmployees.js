af443935f6d6a319cf9fa4d06eec2e3a1a5062ba
Mauricio-OC <murisc0@hotmail.com>
2022-08-06 11:40:59 -0300

    corrigindo primeiro funcao e dando continuacao

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,15 +1,16 @@
 const { employees } = require('../data/zoo_data');
 
 function isManager(id) {
-  return employees.some((employe) => (
-    employe.managers.some((manager) => manager === id)
-  ));
+  const manager1 = employees.some((manager) => manager.managers.includes(id));
+  return manager1;
 }
 
 function getRelatedEmployees(managerId) {
-  if (isManager(managerId) === false) {
-    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  if (isManager(managerId)) {
+    const employe = employees.filter((element) => element.managers.includes(managerId));
+    const names = employe.map((element) => `${element.firsName} ${element.lastName}`)
   }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };

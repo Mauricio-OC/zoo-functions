8482df3d4064e4645761a211d03d279a3cd5f534
Mauricio-OC <murisc0@hotmail.com>
2022-08-06 09:35:43 -0300

    getspeciesid adicionei funcao

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  if (ids.length === 0) {
+    return [];
+  }
 }
 
 module.exports = getSpeciesByIds;

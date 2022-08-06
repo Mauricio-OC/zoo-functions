87804ccb2bf313a393803a9b9d608ffb4b01250d
Mauricio-OC <murisc0@hotmail.com>
2022-08-06 10:30:18 -0300

    adicionei test para monday

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,3 +1,15 @@
 const getOpeningHours = require('../src/getOpeningHours');
-
-describe('Testes da função getOpeningHours', () => {});
+// {
+// Tuesday: { open: 8, close: 6 },
+// Wednesday: { open: 8, close: 6 },
+// Thursday: { open: 10, close: 8 },
+// Friday: { open: 10, close: 8 },
+// Saturday: { open: 8, close: 10 },
+// Sunday: { open: 8, close: 8 },
+// Monday: { open: 0, close: 0 },
+// }
+describe('Testes da função getOpeningHours', () => {
+  it('Verifica ao receber Monday e 09:00-AM retorna string The zoo is closed', () => {
+    expect(getOpeningHours('Monday', '09:00-AM')).toEqual('The zoo is closed');
+  });
+});

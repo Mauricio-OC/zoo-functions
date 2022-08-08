5960ca433b4785e3d4c15dc99a7edd024b3b54d9
Mauricio-OC <murisc0@hotmail.com>
2022-08-08 14:32:19 -0300

    count animals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  const { species } = data;
+  const totalResident = {};
+  if (!animal) {
+    species.forEach((specie) => {
+      totalResident[specie.name] = specie.residents.length;
+    });
+    return totalResident;
+  }
+  const { specie, sex } = animal;
+  const animais = species.find((animalT) => animalT.name === specie);
+  if (sex) {
+    return animais.residents.filter((elemento) => elemento.sex === sex).length;
+  }
+  return animais.residents.length;
 }
 
 module.exports = countAnimals;

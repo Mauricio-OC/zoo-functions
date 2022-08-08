ff7953161c3e49720535e90dc401f26327c27ab4
Mauricio-OC <murisc0@hotmail.com>
2022-08-08 13:43:26 -0300

    finalizando func calculate entry

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,13 +1,20 @@
+const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
   const adulto = entrants.filter((entrantsAdulto) => entrantsAdulto
     .age >= 18 && entrantsAdulto.age < 50);
   const crianca = entrants.filter((entrantsCrianca) => entrantsCrianca.age < 18);
+  const idoso = entrants.filter((entrantsSenior) => entrantsSenior.age >= 50);
+  return { crianca: crianca.length, adulto: adulto.length, idoso: idoso.length };
 }
 
 function calculateEntry(entrants) {
-
+  if (!entrants || Object.keys(entrants).length === 0) { return 0; }
+  const Total = countEntrants(entrants);
+  const precoSenior = prices.senior * Total.senior;
+  const precoAdulto = prices.adult * Total.adult;
+  const precoCrianca = prices.child * Total.child;
+  return precoAdulto + precoCrianca + precoSenior;
 }
-
 module.exports = { calculateEntry, countEntrants };

cd7ba7fc1e6c76e5bcf5eb8ff8e2fe3c0a8c70a2
Mauricio-OC <murisc0@hotmail.com>
2022-08-08 14:48:44 -0300

    getolder v

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,12 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const animal1 = employees.find((people) => people.id === id)
+    .responsibleFor[0];
+  const animal = species.find((spec) => animal1 === spec.id).residents;
+  const older = animal.sort((a, b) => b.age - a.age);
+  return Object.values(older[0]);
 }
 
 module.exports = getOldestFromFirstSpecies;

99eb82f9c9e45d92a326494b843959165815bc47
Mauricio-OC <murisc0@hotmail.com>
2022-08-09 10:12:28 -0300

    calculateentry

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,9 +1,31 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
+  return entrants.reduce((acc, { age }) => {
+    if (age < 18) {
+      acc.child += 1;
+    } else if (age >= 18 && age < 50) {
+      acc.adult += 1;
+    } else {
+      acc.senior += 1;
+    }
+    return acc;
+  }, { child: 0, adult: 0, senior: 0 });
 }
 
 function calculateEntry(entrants) {
-
+  if (!entrants) {
+    return 0;
+  }
+  if (Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const elemento = countEntrants(entrants);
+  const { prices: { child: child1, adult: adult1, senior: senior1 } } = data;
+  const { child: child2, adult: adult2, senior: senior2 } = elemento;
+  const childPrice = child1 * child2;
+  const seniorPrice = senior1 * senior2;
+  const adultPrice = adult1 * adult2;
+  return childPrice + adultPrice + seniorPrice;
 }
 module.exports = { calculateEntry, countEntrants };

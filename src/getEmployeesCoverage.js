6178259eb5d4a902f1c71557cc6515b7325418c9
Mauricio-OC <murisc0@hotmail.com>
2022-08-09 11:50:20 -0300

    employeecoverage

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,36 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const rotina = () => employees.map((element) => {
+  const animals = species.filter((element2) =>
+    element.responsibleFor.find((element3) => element3 === element2.id));
+  const informacao = {
+    id: element.id,
+    fullName: `${element.firstName} ${element.lastName}`,
+    species: animals.map((element2) => element2.name),
+    locations: animals.map((element3) => element3.location),
+  };
+  return informacao;
+});
+
+function getEmployeesCoverage(objeto) {
+  if (!objeto) {
+    return rotina();
+  }
+  const objetoValue = Object.values(objeto).toString();
+  const employee = employees.find((element) => Object.values(element).includes(objetoValue));
+  if (employee === undefined) {
+    throw new Error('Informações inválidas');
+  }
+  const animals = species
+    .filter((element) => employee.responsibleFor.find((eleid) => eleid === element.id));
+  const informacoes = {
+    id: employee.id,
+    fullName: `${employee.firstName} ${employee.lastName}`,
+    species: animals.map((element2) => element2.name),
+    locations: animals.map((element3) => element3.location),
+  };
+  return informacoes;
 }
 
 module.exports = getEmployeesCoverage;
